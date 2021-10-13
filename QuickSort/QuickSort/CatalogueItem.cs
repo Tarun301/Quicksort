c36593175b4607a0000867f4034a6e2c283ba700
Tarun301 <65336900+Tarun301@users.noreply.github.com>
2021-10-13 15:19:45 +1300

    Add files via upload

---
+++ QuickSort/QuickSort/CatalogueItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class CatalogueItem
+    {
+        public Int32 Id { get; set; }
+        public String ItemName { get; set; }
+        public String Category { get; set; }
+
+
+        // constructor
+        public CatalogueItem(Int32 newId, String newName, String newCategory)
+        {
+
+            Id = newId;
+            ItemName = newName;
+            Category = newCategory;
+        }
+    }
+}
